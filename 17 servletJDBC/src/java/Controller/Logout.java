50a8c5592721961a9eb3f52981aa10af10e67d4a
bala418 <balavtwo@gmail.com>
2022-12-21 06:25:56 +0530

    add files

---
+++ 17 servletJDBC/src/java/Controller/Logout.java
@@ -0,0 +1,22 @@
+package Controller;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+public class Logout extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Cookie empNo = new Cookie("empNo", "");
+        response.addCookie(empNo);
+        response.sendRedirect(request.getContextPath() + "/");
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
